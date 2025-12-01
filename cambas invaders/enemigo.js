9118cbc3c7729895038fe6347c817d25b4561ef3
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-12-01 08:36:22 -0400

    Update enemigo.js

--- cambas invaders/enemigo.js
+++ cambas invaders/enemigo.js
@@ -1,4 +1,6 @@
-class Enemigo {
+import BalaEnemigo from "./balaenemigo.js";
+
+export default class Enemigo {
   constructor(matriz, filas, columnas, celdaSize, balasAlien) {
     this.matriz = matriz;
     this.filas = filas;
@@ -15,18 +17,19 @@ class Enemigo {
     }
   }
 
-  disparar(Bala) {
+  disparar() {
     for (let j = 0; j < this.filas; j++) {
       for (let i = 0; i < this.columnas; i++) {
         if (this.matriz.obtener(i, j) === 'alien' && Math.random() < 0.06) {
           const px = i * this.celdaSize + this.celdaSize / 2 - 3;
           const py = j * this.celdaSize + this.celdaSize;
-          this.balasAlien.push(new Bala(px, py, 1, 'alien'));
+          this.balasAlien.push(new BalaEnemigo(px, py, 1, 'alien'));
+
         }
       }
     }
   }
 }
 
-export default Enemigo;
+
 
