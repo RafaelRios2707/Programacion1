39fc9cfa712097aacd58e44d478fd2f0d7af1b85
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-12-01 22:14:24 -0400

    Update balaenemigo.js

--- cambas invaders/balaenemigo.js
+++ cambas invaders/balaenemigo.js
@@ -17,14 +17,16 @@ export default class BalaEnemigo {
   }
 
   dibujar(ctx) {
-    ctx.fillStyle = '#f00'; // rojo para distinguir
+    ctx.fillStyle = this.tipo === 'nave' ? '#0f0' : '#f00'; // verde para nave, rojo para alien
     ctx.fillRect(this.x, this.y, this.width, this.height);
-    // o usa sprite: ctx.drawImage(this.imagen, this.x, this.y, this.width, this.height);
-  }
+    console.log("Dibujando bala en:", this.x, this.y);
+}
+
 
   fueraDelCanvas(canvas) {
     return this.y < 0 || this.y > canvas.height;
   }
 }
+
 
 
