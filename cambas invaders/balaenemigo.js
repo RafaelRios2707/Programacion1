64650e6b6ffa5467fbdd3c0f403b765ab4282819
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-12-01 22:18:33 -0400

    Refactor BalaEnemigo class with updated dimensions

--- cambas invaders/balaenemigo.js
+++ cambas invaders/balaenemigo.js
@@ -2,14 +2,11 @@ export default class BalaEnemigo {
   constructor(x, y, direccion, tipo) {
     this.x = x;
     this.y = y;
-    this.direccion = direccion; // +1 abajo
-    this.tipo = tipo; // 'alien'
+    this.direccion = direccion; // +1 para abajo
+    this.tipo = tipo;
     this.velocidad = 4;
-    this.width = 12;
-    this.height = 24;
-
-    this.imagen = new Image();
-    this.imagen.src = 'assets/icon2.png'; // puedes usar otro sprite si quieres
+    this.width = 10;
+    this.height = 20;
   }
 
   mover() {
@@ -17,16 +14,12 @@ export default class BalaEnemigo {
   }
 
   dibujar(ctx) {
-    ctx.fillStyle = this.tipo === 'nave' ? '#0f0' : '#f00'; // verde para nave, rojo para alien
+    ctx.fillStyle = '#f00'; // rojo para enemigo
     ctx.fillRect(this.x, this.y, this.width, this.height);
-    console.log("Dibujando bala en:", this.x, this.y);
-}
-
+    console.log("Dibujando bala alien en:", this.x, this.y);
+  }
 
   fueraDelCanvas(canvas) {
     return this.y < 0 || this.y > canvas.height;
   }
 }
-
-
-
