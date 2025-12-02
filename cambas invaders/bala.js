ab942cfcd0750056c6e0e58888007e78d0a1cdf7
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-12-01 22:17:59 -0400

    Update bala.js

--- cambas invaders/bala.js
+++ cambas invaders/bala.js
@@ -2,14 +2,11 @@ export default class Bala {
   constructor(x, y, direccion, tipo) {
     this.x = x;
     this.y = y;
-    this.direccion = direccion; // -1 arriba, +1 abajo
-    this.tipo = tipo; // 'nave'
-    this.velocidad = 6;
-    this.width = 20;
-    this.height = 30;
-
-    this.imagen = new Image();
-    this.imagen.src = 'assets/icon2.png'; // tu sprite de bala
+    this.direccion = direccion; // -1 para arriba
+    this.tipo = tipo;
+    this.velocidad = 5;
+    this.width = 10;
+    this.height = 20;
   }
 
   mover() {
@@ -20,16 +17,9 @@ export default class Bala {
     ctx.fillStyle = this.tipo === 'nave' ? '#0f0' : '#f00'; // verde para nave, rojo para alien
     ctx.fillRect(this.x, this.y, this.width, this.height);
     console.log("Dibujando bala en:", this.x, this.y);
-}
-
-
+  }
 
   fueraDelCanvas(canvas) {
     return this.y < 0 || this.y > canvas.height;
   }
 }
-
-
-
-
-
