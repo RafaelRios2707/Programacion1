89ca959294aa64df88836c97bcc9c5d1354c27bc
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-12-10 13:01:08 -0400

    Update game.js

--- cambas invaders/game.js
+++ cambas invaders/game.js
@@ -17,7 +17,7 @@ let alienCargado = false;
 let fondoCargado = false;
 
 alienImg.src = 'assets/alien.png';
-fondoImg.src = 'assets/fondo.png';
+fondoImg.src = 'assets/fondo1.5.png';
 
 alienImg.onload = () => {
   alienCargado = true;
@@ -138,3 +138,4 @@ function gameLoop() {
 
   requestAnimationFrame(gameLoop);
 }
+
